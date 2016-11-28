0efddc730fcb0e7bec1dac80cb2496f6d7152279
ArgentumWalker <svidchen-ko@amil.ru>
2016-11-28 19:12:10 +0300

    Basic threads interface and thread synchronie and lock functions

---
+++ inc/threads.h
@@ -0,0 +1,51 @@
+#ifndef _THREADS_H_
+#define _THREADS_H_
+
+#define MAX_THREAD_COUNT 100000
+
+struct ThreadInfo {
+    int id;
+    int canSwitchThread;
+    int threadLockCount;
+    void* stackPtr;
+}
+
+extern ThreadInfo currentThread;
+
+ThreadInfo* newThread();
+void finishThread(ThreadInfo* thread);
+void switchThread();
+
+#define beginSyncronize()\
+    static struct Mutex* __syncronizeMutex = newMutex();\
+    lock(__syncronizeMutex)
+    
+#define endSynchronize()\
+    unlock(__synchronizeMutex)
+    
+#define lockThread() \
+    beginSynchronize();\
+    currentThread->canSwitchThread = 0;\
+    currentThread->threadLockCount++
+    
+#define unlockThread() \
+    currentThread->threadLockCount--;\
+    if (currentThread->threadLockCount < 0) {\
+        currentThread->threadLockCount = 0;\
+    }\
+    if (currentThread->threadLockCount == 0) {\
+        currentThread->canSwitchThread = 1;\
+    }\
+    endSynchronize()
+
+struct Mutex {
+    int claim[MAX_THREAD_COUNT];
+    int turn[MAX_THREAD_COUNT];
+}
+
+struct Mutex* newMutex();
+void lock(struct Mutex* m);
+void unlock(struct Mutex* m);
+void freeMutex(struct Mutex* m)
+
+#endif
